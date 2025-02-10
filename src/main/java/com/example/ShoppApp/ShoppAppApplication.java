5aebd7339cdcf54be32421fcaa0da14ecbfe52b0
truongsonkmhd <truongsonkmhd@gmail.com>
2025-02-10 23:23:01 +0700

    init_prj

---
+++ src/main/java/com/example/ShoppApp/ShoppAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShoppApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppAppApplication.class, args);
+	}
+
+}
